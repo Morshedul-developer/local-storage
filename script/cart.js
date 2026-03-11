1ef08cfa7e9d10fe31e44ad6796df2eb63209fc6
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-03-11 12:44:09 +0600

    made a simple cart function

--- script/cart.js
+++ script/cart.js
@@ -1,15 +1,28 @@
-const product = document.getElementById("product");
-const quantity = document.getElementById("quantity");
+const productEl = document.getElementById("product");
+const quantityEl = document.getElementById("quantity");
 
 const getProduct = () => {
-  const productValue = product.value;
-  const quantityValue = quantity.value;
+  const product = productEl.value;
+  const quantity = quantityEl.value;
 
   const productContainer = document.getElementById("product-container");
   const li = document.createElement("li");
-  li.innerText = `${productValue} : ${quantityValue}`;
+  li.innerText = `${product} : ${quantity}`;
   productContainer.append(li);
+
+  setToCart(product,quantity);
 
   product.value = "";
   quantity.value = "";
 };
+
+const getCart = () => {
+    const cart = {};
+    return cart;
+}
+
+const setToCart = (product,quantity) => {
+    const cart = getCart();
+    cart[product] = quantity;
+    console.log(cart);
+}
